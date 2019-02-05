f6338d81c8c05fa81d0ad27aed93d519327e3390
Aleksei Ustiuzhanin <aleksei@workpop.com>
2019-02-04 17:59:55 -0800

    Setup Storybook

---
+++ .storybook/config.js
@@ -0,0 +1,20 @@
+import { configure, addDecorator } from '@storybook/react';
+import { withOptions } from '@storybook/addon-options';
+import { checkA11y } from '@storybook/addon-a11y';
+
+addDecorator(
+  withOptions({
+    sortStoriesByKind: true,
+  }),
+);
+
+addDecorator(checkA11y);
+
+// automatically import all files ending in *.stories.tsx
+console.log(20);
+const req = require.context('../stories', true, /.stories.tsx/);
+function loadStories() {
+  req.keys().forEach((filename) => req(filename));
+}
+
+configure(loadStories, module);
